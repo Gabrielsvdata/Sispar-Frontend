32a7d651c46878e008de6b9ac0b9c98737a3f302
Gabrielsvdata <gabrielsilvanovieira7@gmail.com>
2025-03-03 22:01:58 -0300

    alteração no nome da pastas pra pra atualizar o vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login/Login.jsx";
+import Login from "./components/login/Login.jsx";
 import Reembolsos from "./components/reembolsos/Reembolsos.jsx";
 import Solicitacao from "./components/solicitacao/Solicitacao.jsx";
 import "./global.scss";
