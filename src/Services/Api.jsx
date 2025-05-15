ba5775f989a61b6bf7baf3dc64ac8f94d6bc334a
Gabrielsvdata <gabrielsilvanovieira7@gmail.com>
2025-05-15 20:43:04 -0300

    alteração do codigo

--- src/Services/Api.jsx
+++ src/Services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"; //importar a biblioteca axios, que é usada para fazer requisições, HTTP.
 
 const api = axios.create({
-     baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000" // define a url para todas as requisições com essa instancia 
+     baseURL: "http://localhost:5000" // define a url para todas as requisições com essa instancia 
 });
 
 
